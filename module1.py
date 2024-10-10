4cc14efa0ef7fee54cc7420b00cfd3fe0c3f2a8b
mhend-rde213 <mhend.redouane@stjoavignon.com>
2024-10-10 11:40:36 +0200

    module1.py

--- module1.py
+++ module1.py
@@ -1,2 +1,14 @@
 # Créé par elevessi, le 10/10/2024 en Python 3.7
-ggg
+mot=str(input("lettre codeuse"))
+chaine="abcdefghijklmnopqrstuvwxyz"
+nombre=[]
+def freq():
+    if freq(2):
+        for chaine in freq:
+            if mot in chaine:
+                nombre=[1+chaine.index(chaine)]
+return nombre
+
+print(nombre)
+
+#code invalid pour l'instant
