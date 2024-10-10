57b5c3aa5dcc7ac6dc5804d6a224db25786c2e62
floriepeytier <florie.peytier@stjoavignon.com>
2024-10-10 11:02:47 +0200

    Update module3.py

--- module3.py
+++ module3.py
@@ -1,2 +1,8 @@
 # Créé par elevessi, le 10/10/2024 en Python 3.7
-
+# Décoder un message 
+lettre=int(input("lettre codeuse"))
+messagecode=input("Quel messsage souhaitez vous décoder ?")
+for i in mess:
+    nouv_mess=int(ord(mess[i]))
+    mess.replace([i-1],chr(nouv_mess-lettre))
+print(mess)
