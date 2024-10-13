081c43a31360aef6b8ffcff2eee115d1f98273bd
floriepeytier <florie.peytier@stjoavignon.com>
2024-10-13 18:39:01 +0200

    Update module3.py

--- module3.py
+++ module3.py
@@ -1,28 +1,23 @@
-# Créé par elevessi, le 10/10/2024 en Python 3.7
-# Décoder un message 
+# Décoder un message
 alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lettre=5
+lettre=6
 mess_decode=[]
-mess=input("Quel messsage souhaitez vous décoder ?")  
+mess=input("Quel messsage souhaitez vous décoder ?")
 c=0
-try: 
+try:
     while c==0:
-    for i in mess:
-        if i in alphabet:
-            c=1
-        else:
-            mess=input("Vous devez entrer des lettres en majuscule !  Message à décoder :")
+        for i in mess:
+            if i in alphabet:
+                c=1
+            else:
+                mess=input("Vous devez entrer des lettres en majuscule !  Message à décoder :")
 except:
     pass
-        
-mess
-mess_decode=[]
 
-for i in mess:
-    i=1
-    ord(mess[0-i])
-    nouv=ord(mess[0-i])-lettre
+taille=len(mess)
+for i in range (0,taille,1):
+    ord(mess[0+i])
+    nouv=ord(mess[0+i])-lettre
     remplacer=chr(nouv)
     mess_decode.append(remplacer)
-    i+=1
 print("".join(mess_decode))
