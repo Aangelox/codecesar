34c356710311ebaa5f0e18b74e299f1a97657d73
floriepeytier <florie.peytier@stjoavignon.com>
2024-10-17 09:06:09 +0200

    Update module3.py

--- module3.py
+++ module3.py
@@ -1,28 +1,36 @@
 # Décoder un message
-alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lettre=6
-mess_decode=[]
-mess=input("Quel messsage souhaitez vous décoder ?")
-c=0
-try:
-    while c==0:
-        for i in mess:
-            if i in alphabet:
-                c=1
-            else:
-                mess=input("Vous devez entrer des lettres en majuscule !  Message à décoder :")
-except:
-    pass
-
-taille=len(mess)
-for i in range (0,taille,1):
-    if ord(mess[0+i]) >91 and ord(mess[0+i])<33: 
-        nouv=ord(mess[0+i])-lettre + (26-lettre)
-    else : 
-        ord(mess[0+i])
-    remplacer=chr(nouv)
-    mess_decode.append(remplacer)
-print("".join(mess_decode))
+def decodage (message, lettre):
+    """
+    Permet le décodage d'un message codé à l'aide d'un code César.
+    Utilisation : rentrer le message à décoder entre guillemets, puis le numéro de la lettre codeuse.
+    """
+    message_decode=[]
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    c=0
+    message_erreur="Vous devez entrer des lettres en majuscule ! Message à décoder :"
+    try:
+        while c==0:
+            for i in message :
+                if i in alphabet:
+                    c=1
+                else:
+                    message=input(message_erreur)
+                    c=0
+    except:
+        c=0
+        message=input(message_erreur)
+    taille_message=len(message)
+    for i in range (0,taille_message,1):
+        if ord(message [0+i])<(65+lettre)and ord(message [0+i])>64: 
+            nouvelle_lettre=ord(message[0+i])-lettre + 26
+            nouvelle_lettre=chr(nouvelle_lettre)
+        else : 
+            ord(mess[0+i])
+            nouvelle_lettre=ord(message[0+i])-lettre
+        message_decode.append(nouvelle_lettre)
+    print("".join(mess_decode))
+    return message_decode
+print(decodage("",1))
 
 #########################
 
