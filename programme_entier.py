304a5fc9b6d2c9b97687aa9c2d24afe0d35c52af
floriepeytier <florie.peytier@stjoavignon.com>
2024-10-31 16:07:06 +0100

    Update programme_entier.py

--- programme_entier.py
+++ programme_entier.py
@@ -1,5 +1,39 @@
 #Code César
 ##### FONCTIONS
 # Décodage
+def decodage(message, lettre):#decalage = de la lettre codeuse
+    """
+    Permet le décodage d'un message codé à l'aide d'un code César.
+    Utilisation : rentrer le message à décoder entre "", puis le numéro de la lettre codeuse.
+    """
 
+    message_decode=[]
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    message_erreur="Vous devez entrer des lettres en majuscule !  Message à décoder :"
+    c=0
+    try:
+        while c==0:
+            for i in message:
+                if i in alphabet:
+                    c=1
+                else:
+                    message=input(message_erreur)
+                    c=0
+    except:
+        message=input(message_erreur)
+        c=0
+
+
+    taille_message=len(message)
+    for i in range (0,taille_message,1):
+        if ord(message[0+i]) <(65+lettre) and ord(message[0+i])>64:
+            nouvelle_lettre=ord(message[0+i])-lettre + 26
+            nouvelle_lettre=chr(nouvelle_lettre)
+        else :
+            ord(message[0+i])
+            nouvelle_lettre=ord(message[0+i])-lettre
+            nouvelle_lettre=chr(nouvelle_lettre)
+        message_decode.append(nouvelle_lettre)
+    message_decode="".join(message_decode)
+    return message_decode
 ############# PROGRAMME PRINCIPAL
