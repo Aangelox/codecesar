fc591f218305eb93d417cebfdb098ad0ba797f49
floriepeytier <florie.peytier@stjoavignon.com>
2024-11-02 17:26:51 +0100

    Update module1.py

--- module1.py
+++ module1.py
@@ -12,3 +12,23 @@ return nombre
 print(nombre)
 
 #code invalid pour l'instant
+#############
+#Voilà ce que j'ai fait, dites moi ce que vous en pensez.
+# LETTRE CODEUSE
+def lettre_codeuse () :
+    alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    phrase=input("Donner la phrase qui constitue la lettre codeuse.")
+    phrase=phrase.split(" ")
+    taille = len(phrase)
+    for i in range (taille):
+        for j in range (len(phrase[i])-1):
+            iterations=phrase[i].count(phrase[i][j])
+            if iterations ==2:
+                lettre_codeuse=phrase[i][j]
+                lettre_codeuse=lettre_codeuse.upper()
+                code=alphabet.index(lettre_codeuse)+1
+                break
+            else:
+                j+=1
+        i=i+1
+    return (lettre_codeuse, code)
