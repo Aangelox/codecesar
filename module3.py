1497c2a326e13a108d9ce5f581cbf9e80ddb34ab
floriepeytier <florie.peytier@stjoavignon.com>
2024-11-02 14:39:00 +0100

    Update module3.py

--- module3.py
+++ module3.py
@@ -1,38 +1,48 @@
-# Décoder un message
-def decodage (message, lettre):
+def decodage(message, lettre):#decalage = de la lettre codeuse
     """
     Permet le décodage d'un message codé à l'aide d'un code César.
-    Utilisation : rentrer le message à décoder entre guillemets, puis le numéro de la lettre codeuse.
+    Utilisation : rentrer le message à décoder entre "", puis le numéro de la lettre codeuse.
     """
     message_decode=[]
-    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ' "
+    message_erreur="Vous devez entrer des lettres en majuscule !  Message à décoder :"
     c=0
-    message_erreur="Vous devez entrer des lettres en majuscule ! Message à décoder :"
     try:
         while c==0:
-            for i in message :
+            for i in message:
                 if i in alphabet:
                     c=1
                 else:
                     message=input(message_erreur)
                     c=0
     except:
-        c=0
         message=input(message_erreur)
+        c=0
+
     taille_message=len(message)
     for i in range (0,taille_message,1):
-        if ord(message [0+i])<(65+lettre)and ord(message [0+i])>64: 
+        if (ord(message[0+i])==32) :
+            nouvelle_lettre=' '
+        elif (ord(message[0+i])==39):
+            nouvelle_lettre="'"
+        elif ord(message[0+i]) <(65+lettre) and ord(message[0+i])>64:
             nouvelle_lettre=ord(message[0+i])-lettre + 26
             nouvelle_lettre=chr(nouvelle_lettre)
-        else : 
-            ord(mess[0+i])
+        else :
+            ord(message[0+i])
             nouvelle_lettre=ord(message[0+i])-lettre
+            nouvelle_lettre=chr(nouvelle_lettre)
         message_decode.append(nouvelle_lettre)
-    print("".join(mess_decode))
+    message_decode="".join(message_decode)
     return message_decode
-print(decodage("",1))
+#BOUCLE TEST
+for i in range (27):
+    print (i, decodage("ABCDEFGHIJK LMN'OPQRSTUVWXYZ",i))
+
+print (decodage("UT YK XKZXUABK IUSSK IUTBKTA PKAJO G YKVZ NKAXKY PK VUXZKXGO AT INGVKGA BKXZ KZ AT VGXGVRAOK HRKA RK ZGHRKGA KYZ KT YKIAXOZK OR KYZ OTAZORK JK INKXINKX G S'KTZUAXRUAVKX SKZZXK R'GXMKTZ JGTY AT YGI TUOX KZ BKTKF YKAR",6))
 
 #########################
+# Normalement le code fonctionne, je n'ai pas réussi à le faire avec la méthode .replace mais ça marche quand même.
 
 #jsp si ton code fonctionne mais personnellement jsuis parti sur quelquqe chose comme ca
 # Décoder un message
